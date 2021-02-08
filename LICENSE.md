72954399de3918a6a0dd90575b37c3e1dc72162b
Jason <jason@casjay.net>
2021-02-07 19:11:23 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

---
+++ LICENSE.md
@@ -0,0 +1,13 @@
+         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE  
+                Version 2, December 2004  
+
+          Copyright (C) 2021 Jason Hempstead
+  
+Everyone is permitted to copy and distribute verbatim or modified copies of  
+this license document, and changing it is allowed as long as the name is changed.  
+  
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE  
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION  
+  
+0. You just DO WHAT THE FUCK YOU WANT TO.  
+1. End of the What The Fuck Public License
---
+++ install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="dwt1"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 020720211910-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.com
+# @License       : LICENSE.md
+# @ReadME        : README.md
+# @Copyright     : Copyright: (c) 2021 Jason Hempstead, CasjaysDev
+# @Created       : Sunday, Feb 07, 2021 19:10 EST
+# @File          : install.sh
+# @Description   : Wallpapers from gitlab.com/dwt1
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/master/functions}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the main function
+system_installdirs
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Make sure the scripts repo is installed
+scripts_check
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Defaults
+APPNAME="${APPNAME:-dwt1}"
+APPDIR="${WALLPAPERS:-$SHARE/wallpapers}/$APPNAME"
+INSTDIR="$SHARE/CasjaysDev/installed/$SCRIPTS_PREFIX/$APPNAME"
+REPO="${WALLPAPERMGRREPO}"
+REPORAW="$REPO/$APPNAME/raw"
+APPVERSION="$(__appversion ${REPO:-https://github.com/$SCRIPTS_PREFIX}/$APPNAME/raw/master/version.txt)"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the wallpapermgr function
+wallpapermgr_install
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Script options IE: --help
+show_optvars "$@"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ensure directories exist
+ensure_dirs
+ensure_perms
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main progam
+if [ -d "$APPDIR/.git" ]; then
+  execute \
+    "git_update $APPDIR" \
+    "Updating $APPNAME configurations"
+else
+  execute \
+    "git_clone -q $REPO/$APPNAME $APPDIR" \
+    "Installing $APPNAME configurations"
+fi
+
+# exit on fail
+failexitcode
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run post install scripts
+run_postinst() {
+  wallpapermgr_run_post
+}
+
+execute \
+  "run_postinst" \
+  "Running post install scripts"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# create version file
+wallpapermgr_install_version
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# exit
+run_exit
+# end
